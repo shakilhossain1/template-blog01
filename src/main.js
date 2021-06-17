42d447a7edf8411e5567ed73a1574f4d5a8328f4
Shakil Hossain <sh9743441@gmail.com>
2021-06-17 23:25:14 +0600

    making the blog single page

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './tailwind.css'
+import "alpinejs";
+
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
