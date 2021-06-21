06460adcb1ef1be35f784ab0df8c9223b0b5b4f7
Shakil Hossain <sh9743441@gmail.com>
2021-06-21 11:59:55 +0600

    nav links

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import './tailwind.css'
-import "alpinejs";
+import Alpine from "alpinejs";
 
+window.Alpine = Alpine;
+
+Alpine.start();
 // document.querySelector('#app').innerHTML = `
 //   <h1>Hello Vite!</h1>
 //   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
